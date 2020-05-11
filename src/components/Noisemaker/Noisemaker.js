56d84c49479f97551222c3780dcbbee1b01281bc
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-05-11 13:46:49 -0400

    found correct notes to play made basic button interface

--- src/components/Noisemaker/Noisemaker.js
+++ src/components/Noisemaker/Noisemaker.js
@@ -12,15 +12,56 @@ export default class Noisemaker extends Component {
     }
 
 
-    makeNoiseActive() {
-        this.synth.triggerAttackRelease('C4', 1)
+    handleNotePlay(e) {
+        const {value} = e.value
+        console.log(value)
+    }
+
+    playDo() {
+        this.synth.triggerAttackRelease('C4', '8n')
+    }
+
+    playRe() {
+        this.synth.triggerAttackRelease('D4', '8n')
+    }
+
+    playMi() {
+        this.synth.triggerAttackRelease('E4', '8n')
+    }
+
+
+    playFa() {
+        this.synth.triggerAttackRelease('F4', '8n')
+    }
+
+
+    playSo() {
+        this.synth.triggerAttackRelease('G4', '8n')
+    }
+
+    playLa() {
+        this.synth.triggerAttackRelease('A4', '8n')
+    }
+
+    playTi() {
+        this.synth.triggerAttackRelease('B4', '8n')
+    }
+
+    playDo2() {
+        this.synth.triggerAttackRelease('C5', '8n')
     }
 
     render() {
-        const {noiseActive} = this.state
         return <>
-                
-                <button disabled={noiseActive} onClick={e => this.makeNoiseActive()}></button>
+
+                <button onClick={e => this.playDo()}>DO</button>
+                <button onClick={e => this.playRe()}>RE</button>
+                <button onClick={e => this.playMi()}>MI</button>
+                <button onClick={e => this.playFa()}>FA</button>
+                <button onClick={e => this.playSo()}>SO</button>
+                <button onClick={e => this.playLa()}>LA</button>
+                <button onClick={e => this.playTi()}>TI</button>
+                <button onClick={e => this.playDo2()}>DO</button>
         </>
     }
 }
